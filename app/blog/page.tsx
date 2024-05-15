cf76398ce167c5aa3b426c13d7f5b7ec7a412bb6
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2024-05-15 07:49:04 -0700

    npm to pnpm and v0.1 blogs from md files working (#2)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,32 @@
+// blog.server.js
 import React from "react";
 import Header from "@/components/Header";
+import { getSortedPosts } from "../../lib/posts";
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await getSortedPosts(); // Asynchronously fetch your posts
   return (
     <>
       <Header />
-      <h2 className="mb-4 text-center text-2xl font-light">Blog Coming Soon</h2>
+      <h2 className="mb-4 text-center text-2xl font-light">
+        Latest Blog Posts (Work in Progress)
+      </h2>
+      <div>
+        {posts.map(
+          (post: {
+            slug: React.Key | null | undefined;
+            title: string;
+            date: string;
+            excerpt: string;
+          }) => (
+            <div key={post.slug}>
+              <h3>{post.title}</h3>
+              <p>{post.date}</p>
+              <p>{post.excerpt}</p>
+            </div>
+          ),
+        )}
+      </div>
     </>
   );
 }
