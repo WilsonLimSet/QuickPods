6146b4e855fd63225cd573f195cc6d73e1189e6b
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2024-05-18 20:31:08 -0700

    pls fix

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import { getSortedPosts } from "@/lib/posts";
+import React from "react";
+import Markdown from "markdown-to-jsx";
+import Header from "@/components/Header";
+
+const getPostContent = (slug: string) => {
+  const posts = getSortedPosts();
+  return posts.find((post: any) => post.slug === slug);
+};
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: {
+  params: any;
+  searchParams: any;
+}) {
+  const id = params?.slug ? " ⋅ " + params?.slug : "";
+  return {
+    title: `Blog Post${id.replaceAll("_", " ")}`,
+  };
+}
+export default function BlogPostPage({ params }: { params: any }) {
+  const slug = params.slug;
+  const post = getPostContent(slug);
+  if (!post)
+    return <div className="mt-20 text-center text-xl">Post not found</div>;
+
+  return (
+    <>
+      <Header />
+      <main className="mx-auto max-w-4xl p-4">
+        <h1 className="text-2xl text-slate-600 ">{post.title}</h1>
+        <p className="mt-2 text-slate-400">{post.date}</p>
+
+        <article>
+          <Markdown>{post.content}</Markdown>
+        </article>
+      </main>
+    </>
+  );
+}
