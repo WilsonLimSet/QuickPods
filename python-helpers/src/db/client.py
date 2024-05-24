96696f206ee80a3bda9a0c204349baaae1449420
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2024-05-24 16:47:27 -0700

    blogs generated from urls

--- python-helpers/src/db/client.py
+++ python-helpers/src/db/client.py
@@ -1,30 +1,65 @@
 import os
 from supabase import create_client, Client
 
+
 class Client:
     def __init__(self):
-        supabase_url = os.getenv('SUPABASE_URL')
-        supabase_key = os.getenv('SUPABASE_KEY')
+        supabase_url = os.getenv("SUPABASE_URL")
+        supabase_key = os.getenv("SUPABASE_KEY")
         self.cli: Client = create_client(supabase_url, supabase_key)
         self.table_name = "Podcasts"
-    
+
     def upload(self, data):
         self.cli.table(self.table_name).insert(data).execute()
-    
+
+    def update_blog_generated(self, url):
+        """Update the 'blog_generated' status to True for a specific YouTube URL."""
+        update_result = (
+            self.cli.table(self.table_name)
+            .update(
+                {
+                    "blog_generated": True  # Set the field to True
+                }
+            )
+            .eq("youtube_url", url)
+            .execute()
+        )  # Match the row where 'youtube_url' equals the given URL
+
+    def get_unprocessed_urls(self):
+        """Retrieve YouTube URLs and slugs from the database where blog_generated is false."""
+
+        query_result = (
+            self.cli.table(self.table_name)
+            .select("youtube_url, md_slug, publish_date")
+            .eq("blog_generated", False)
+            .execute()
+        )
+        print(query_result)
+        # Check directly if there's an error in the response
+        # if query_result.error:
+        #     raise Exception(
+        #         f"Failed to fetch unprocessed URLs: {query_result.error_description}"
+        #     )
+        return [
+            (item["youtube_url"], item["md_slug"], item["publish_date"])
+            for item in query_result.data
+        ]
+
     def url_exists(self, url: str):
         def standardize_url(url: str):
-            if url.startswith('http://www.'):
-                url = 'https://' + url[len('http://www.'):]
-            elif url.startswith('http://'):
-                url = 'https://' + url[len('http://'):]
-            elif url.startswith('https://www.'):
-                url = 'https://' + url[len('https://www.'):]
-            elif url.startswith('https://'):
+            if url.startswith("http://www."):
+                url = "https://" + url[len("http://www.") :]
+            elif url.startswith("http://"):
+                url = "https://" + url[len("http://") :]
+            elif url.startswith("https://www."):
+                url = "https://" + url[len("https://www.") :]
+            elif url.startswith("https://"):
                 url = url
             else:
-                url = 'https://' + url
+                url = "https://" + url
 
             return url
+
         TARGET_COL = "youtube_url"
         res = self.cli.table(self.table_name).select(TARGET_COL).execute()
 
@@ -32,4 +67,3 @@ class Client:
         standardized = standardize_url(url)
 
         return standardized in urls
-
--- python-helpers/src/models/model.py
+++ python-helpers/src/models/model.py
@@ -1,33 +1,68 @@
 import os
 import google.generativeai as genai
+import time
+
 
 class Model:
     def __init__(self):
-        google_api_key = os.getenv('GOOGLE_API_KEY')
+        google_api_key = os.getenv("GOOGLE_API_KEY")
         genai.configure(api_key=google_api_key)
         GEMINI_MODEL = "models/gemini-1.5-pro-latest"
-        
-        self.model = genai.GenerativeModel(GEMINI_MODEL)
-    
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_DANGEROUS",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_NONE",
+            },
+        ]
+
+        self.model = genai.GenerativeModel(
+            GEMINI_MODEL, safety_settings=safety_settings
+        )
+
     def get_response(self, query: str):
         try:
             res = self.model.generate_content([query])
             if not res:
                 return ""
-            
+
             return res.text
         except Exception as e:
             print(f"Model.get_response: {str(e)}")
             return ""
-    
+
     def execute_prompt_from_audio(self, prompt: str, audio_file_path):
-        try:
-            file_id = genai.upload_file(path=audio_file_path)
-            res = self.model.generate_content([prompt, file_id])
-            if not res:
-                return ""
-        
-            return res.text
-        except Exception as e:
-            print(f"Model.get_insights_from_audio: {str(e)}")
-            return ""
+        retries = 3  # Number of retries
+        for attempt in range(retries):
+            try:
+                file_id = genai.upload_file(path=audio_file_path)
+                res = self.model.generate_content(
+                    [prompt, file_id], request_options={"timeout": 1000}
+                )
+                if res:
+                    time.sleep(1)
+                    return res.text
+
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed: {str(e)}")
+                print(res.prompt_feedback)
+                if attempt < retries - 1:
+                    print("Retrying...")
+                else:
+                    print("All attempts failed.")
+        return ""
