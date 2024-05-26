7509289543209ff2e49a813567fbf950539dcbb5
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2024-05-25 23:23:43 -0700

    third try

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-s">
+    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-s text-black">
       <p>
         Built by{" "}
         {/* <a
