6235b19473e43d001f13323c9b2beb0fd8f67c7a
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2024-05-25 23:20:44 -0700

    second  try

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,11 @@ export default function About() {
   return (
     <>
       <Header />
-      <h2 className="my-8 text-center text-2xl font-bold text-black dark:text-white">
+      <h2 className="my-8 text-center text-2xl font-bold text-black">
         Effortless Podcast Discovery 🎧
       </h2>
       <div className="mx-auto max-w-4xl px-4 py-8">
-        <p className="mb-4 text-center text-lg  text-black dark:text-white">
+        <p className="mb-4 text-center text-lg  text-black ">
           Welcome to CEO Insights, where we make discovering your next favorite
           podcast effortless through generating summaries for you to easily read
           through and determine which one is actually worth your time. 🌟
