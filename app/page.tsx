902dc8fc66205a85c84972c7039cecc2617460b4
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2024-05-30 16:33:28 -0700

    sort by date

--- app/page.tsx
+++ app/page.tsx
@@ -21,37 +21,52 @@ interface CardData {
 
 export default function Index() {
   const [cards, setCards] = useState<CardData[]>([]);
+  const [sortOrder, setSortOrder] = useState("ASC"); // 'ASC' or 'DESC'
 
   useEffect(() => {
     const init = async () => {
       try {
         const supabase = createClient();
-        const { data, error } = await supabase.from("Podcasts").select("*");
+        let query = supabase.from("Podcasts").select("*");
+
+        if (sortOrder === "ASC") {
+          query = query.order("publish_date", { ascending: true });
+        } else {
+          query = query.order("publish_date", { ascending: false });
+        }
+
+        const { data, error } = await query;
         if (error) throw error;
         if (data) {
           setCards(data);
-          console.log(data);
         }
       } catch (e) {
         console.error("Error connecting to Supabase", e);
       }
     };
-
     init();
-  }, []);
+  }, [sortOrder]);
 
   return (
     <>
       <Header />
-      {/* <main className="flex w-full flex-1 flex-col items-center py-10"> */}
-      <main className="flex  flex-col items-center py-10">
-        <div className="grid max-w-7xl grid-cols-1 gap-3 px-2  md:grid-cols-2 lg:grid-cols-4">
+      <main className="flex flex-col items-center py-10">
+        <div className="mb-4">
+          <select
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+            className="rounded-md border p-2"
+          >
+            <option value="ASC">Sort by Earliest Date</option>
+            <option value="DESC">Sort by Latest Date</option>
+          </select>
+        </div>
+        <div className="grid max-w-7xl grid-cols-1 gap-3 px-2 md:grid-cols-2 lg:grid-cols-4">
           {cards.map((card, index) => (
             <PodCard key={index} {...card} />
           ))}
         </div>
       </main>
-
       <Footer />
     </>
   );
